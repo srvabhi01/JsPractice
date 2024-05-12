09d499590e8b093ca58012cf342946d25c8c4f8e
srvabhi01 <143097634+srvabhi01@users.noreply.github.com>
2024-05-12 07:58:48 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1001;
+let accountEmail = "abc@test.com";
+var acccountPassword = "12345";
+accountCity = "Patna";
+
+
+// accountId = 2;
+ 
+console.log(accountId);
+console.table([accountId, accountEmail, acccountPassword, accountCity]);
